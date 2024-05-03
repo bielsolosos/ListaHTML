e0d2238a403f0f297d1b9334407614b9e403539c
bielsolosos <bielrochasantoscoutinho@gmail.com>
2024-05-03 11:51:05 -0300

    login 90% funcional e navbar ajustadas

--- PROJETOFINAL/Styles/login.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const loginContainer = document.getElementById('logcontainer')
-
-const moveOverlay = () => loginContainer.classList.toggle('move')
-
-document.getElementById('cadastrar').addEventListener('click', moveOverlay)
-document.getElementById('logar').addEventListener('click', moveOverlay)
--- PROJETOFINAL/js/login.js
+++ PROJETOFINAL/js/login.js
@@ -8,6 +8,16 @@ document.getElementById('logar').addEventListener('click', moveOverlay)
 document.getElementById('open-register-mobile').addEventListener('click', moveOverlay)
 document.getElementById('open-login-mobile').addEventListener('click', moveOverlay)
 
-function perfil(){
-    window.location = "Perfil.html"
+function logar(){
+    let login = document.getElementById('login').value;
+    let senha = document.getElementById('senha').value;
+    
+    console.log(login);
+    console.log(senha);
+    if (login === 'teste@gmail.com' && senha === 'senha123') {
+        alert("Logado com sucesso!");
+        location.href = "../Perfil.html"
+    } else {
+        alert("Login ou senha incorretos."); 
+    }
 }
